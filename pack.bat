0c1a20e776a691710f9dd2633a4a3e227fcaa49c
GamerNoTitle <GamerNoTitle@outlook.com>
2024-12-26 12:55:39 +0800

    Add: more information when building and add a icon

--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
 @echo off
-nuitka --standalone --include-data-dir=templates=templates --include-data-dir=static=static --lto=yes --assume-yes-for-downloads app.py
+nuitka --standalone --include-data-dir=templates=templates --include-data-dir=static=static --windows-icon-from-ico=img/icon.ico --windows-company-name=GamerNoTitle --windows-product-name="GDUT抢课助手" --windows-file-version=1.6 --windows-product-version=1.6 --lto=yes --assume-yes-for-downloads app.py
